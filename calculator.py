e50ae11ba72b163a3d7320af05f53b6d77b29431
Marcos Felipe de Castro Ornelas <mornelas@mocs.flsouthern.edu>
2026-01-17 15:56:06 -0500

    added error handling, like inputing string errors and description for division by 0 and rooting by negative numbers

--- calculator.py
+++ calculator.py
@@ -1,25 +1,37 @@
 import sys
 def add(x, y) :
+    if isinstance(x, str) or isinstance(y, str) :
+        print('Can\'t input a string, must be a number\t')
     return x + y
 
-def sub(x, y) : 
+def sub(x, y) :
+    if isinstance(x, str) or isinstance(y, str) :
+        print('Can\'t input a string, must be a number\t')
     return x - y
 
 def mul(x, y) :
+    if isinstance(x, str) or isinstance(y, str) :
+        print('Can\'t input a string, must be a number\t')
     return x * y 
 
 def div(x, y) :
+    if isinstance(x, str) or isinstance(y, str) :
+        print('Can\'t input a string, must be a number\t')
     if y != 0 :
         return x // y
     else :
-        return "undefined"
+        return "Undefined, can't divide by zero"
 
 def power(x, y) :
+    if isinstance(x, str) or isinstance(y, str) :
+        print('Can\'t input a string, must be a number\t')
     return x ** y
 
 def square_root(x) :
+    if isinstance(x, str):
+        print('Can\'t input a string, must be a number\t')
     if x < 0 :
-        return "imaginary number"
+        return "Imaginary number, can't take root of negative numbers"
     else :
         return int(x ** 0.5)
 
